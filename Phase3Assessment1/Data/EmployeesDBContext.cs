2a230b52986998ab6b444cc7ca610477155e2850
abc <abc.@gmail.com>
2024-02-12 12:45:44 +0530

    Add project files.

---
+++ Phase3Assessment1/Data/EmployeesDBContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Phase3Assessment1.Models;
+
+namespace Phase3Assessment1
+{
+    public class EmployeesDBContext : DbContext
+    {
+        public EmployeesDBContext (DbContextOptions<EmployeesDBContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Phase3Assessment1.Models.EmpProfile> EmpProfile { get; set; } = default!;
+    }
+}
---
+++ Phase3Assessment1/Models/EmpProfile.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Phase3Assessment1.Models
+{
+    [Table("EmpProfile")]
+    public class EmpProfile
+    {
+        [Key]
+        public int EmpCode { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        //[StringLength(100)]
+        public string ?EmpName { get; set; }
+        public string ?Email { get; set; }
+        public int DeptCode { get; set; }
+
+    }
+}
